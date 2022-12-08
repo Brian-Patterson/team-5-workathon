fd3ce61f97b2e25ae16d15feb8c8623878eb57ab
becp12 <rebecca-preece@outlook.com>
2022-12-07 17:44:26 -0800

    Adding back button to all headers on all pages

--- src/components/Headers/FavoritesHeader.jsx
+++ src/components/Headers/FavoritesHeader.jsx
@@ -1,10 +1,18 @@
 import "../Header.css";
-import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function FavoritesHeader() {
-    const { pathname } = useLocation();
+    const navigate = useNavigate();
+
+    function handleClick() {
+        navigate(-1);
+    };
+
     return (
-        <div className="red">
+        <div className="red header-data">
+            <div onClick={handleClick} className="backbtn">
+                <img src="https://i.imgur.com/rIP5ZaN.png" alt="back button" />
+            </div>
             <h1>Favorites</h1>
         </div>
     )
